e4f168fc31797d8e7552ea556536a9df635b00d7
reine <allaboutme-reine@hotmail.com>
2024-10-31 19:38:25 +0200

    Update airbyte source script to work with Google Sheets

--- AirByte_Scripts/main.py
+++ AirByte_Scripts/main.py
@@ -1,5 +1,5 @@
 from building_blocks import test_connection, get_access_token, get_source_definition
-from source import create_google_web_fonts_source
+from source import create_google_sheets_source
 import os
 from dotenv import load_dotenv, find_dotenv
 import json
@@ -10,15 +10,21 @@ load_dotenv(find_dotenv(), override=True)
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECERT = os.getenv('CLIENT_SECRET')
 WORK_SPACE = os.getenv('WORKSPACE_ID')
-WF_KEY = os.getenv('WF_API_KEY')
+SERVICE_ACC = os.getenv('SERVICE_ACC')
+SPREAD_SHEET = os.getenv('SPREAD_ID')
 # print(test_connection())
 
+# print(SPREAD_SHEET)
+
+with open(SERVICE_ACC) as auth_file:
+    credentials = json.load(auth_file)
+
 TOKEN = get_access_token(CLIENT_ID, CLIENT_SECERT)
-SOURCE_DEF = get_source_definition('google webfonts', TOKEN)
+SOURCE_DEF = get_source_definition('google sheets', TOKEN)
 connected = test_connection()
 
 if connected and (TOKEN != "") and (SOURCE_DEF != ""):
-    create_google_web_fonts_source("api_test", WF_KEY, SOURCE_DEF, WORK_SPACE, TOKEN)
+    create_google_sheets_source("API Test", SPREAD_SHEET, "Service", credentials, SOURCE_DEF, WORK_SPACE, TOKEN)
 
 else:
     "Something went wrong"
--- AirByte_Scripts/source.py
+++ AirByte_Scripts/source.py
@@ -1,6 +1,7 @@
 # -- import statments -- #
 
 import requests
+import json
 
 # -- Airbyte API URLS -- #
 
@@ -9,16 +10,19 @@ AIRBYTE_API = "http://localhost:8000/api/v1"
 
 # -- Main Code -- #
 
-def create_google_web_fonts_source(source_name: str, api_key: dict, source_definition: str, workspace: str, token: str):
+def create_google_sheets_source(source_name: str, spreadsheet_id: str, auth_type: str, credentials: dict, source_definition: str, workspace: str, token: str):
     url = AIRBYTE_API + "/sources/create"
 
     payload = {
-        "sourceDefinitionId": source_definition,
+        "name": f"Google Sheets - {source_name}",
         "workspaceId": workspace,
-        "connectionConfiguration":{
-            "api_key": api_key
-        },
-        "name": "Google Web Fonts " + source_name
+        "sourceDefinitionId": source_definition,
+        "connectionConfiguration": {
+            "spreadsheet_id": spreadsheet_id,
+            "credentials": {
+                'auth_type': auth_type,
+                "service_account_info": str(credentials)}
+        }
     }
 
     headers = {
@@ -32,7 +36,8 @@ def create_google_web_fonts_source(source_name: str, api_key: dict, source_defin
         # print(response.json())
         response.raise_for_status()
 
-        print(f"Source for Google Web Font - {source_name} successfully made")
+        print(f"Source for Google Sheets - {source_name} source successfully made")
     
     except Exception as e:
+        print(response.text)
         print(f"{e}")
